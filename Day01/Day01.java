b50d124ad26b6947440380ae418626ce052a5767
jmdd316 <110305692+jmdd316@users.noreply.github.com>
2022-12-01 14:20:15 -0600

    workingDay1Part1

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,12 +1,51 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 public class Day01 {
 
-    public static void part1() {
+    private final static String DATA_FILE = "input.txt";
 
+    public static void part1() {
+        Scanner in;
+        try{
+            in = new Scanner(new File(DATA_FILE));
+        } catch(Exception e) {
+            System.out.println(e);
+            return;
+        }
+        int mostCalories = 0;
+        while (in.hasNext()){
+            String line = in.nextLine();
+            int calories = 0;
+
+            // get calories not separated by a space
+            while(!line.equals("") && in.hasNext()){
+                calories += Integer.parseInt(line);
+
+                    line = in.nextLine();
+
+               
+            }
+
+            if(calories > mostCalories){
+                mostCalories = calories;
+            }
+
+        }
+        System.out.println(mostCalories);
+    
+        
     }
 
 
     public static void part2() {
         
+    }
+
+    public static void main(String[] args){
+        part1();
+
     }
 
 
