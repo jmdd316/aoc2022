d6dba1828932390cd9d7b61c26deb2a7113bfc5c
jmdd316 <110305692+jmdd316@users.noreply.github.com>
2022-12-08 14:14:21 -0600

    da66666

--- Day06/Day06.java
+++ Day06/Day06.java
@@ -9,24 +9,49 @@ public class Day06 {
         int count = 0;
         String line = in.nextLine();
         
-        for(int i = 0; i < line.length(); i++){
-           
-            for(int j=i+1; j< line.length();j++){
-               
-               
-                if (line.charAt(i) == line.charAt(j)){
-                    count = 0;
+        outerLoop:
+        for(int i = 0; i < line.length()-3; i++){
+            String fourWeAreChecking = line.substring(i,i+4);
+            char[] chars = fourWeAreChecking.toCharArray();
+            for(int p = 0; p <chars.length; p++){
+                for(int q = p+1; q < chars.length; q++){
+                    if(chars[p] == chars[q]){
+                        continue outerLoop;
+                    }
                 }
             }
-
-            }
-            System.out.println(count);
+            System.out.println(i+4);
+            break;
+         }
+         
+            
         }
      
 
     public static void part2() throws IOException {
+        Scanner in = new Scanner(new File("sample.txt"));
+        int count = 0;
+        String line = in.nextLine();
+        
+        outerLoop:
+        for(int i = 0; i < line.length()-3; i++){
+            String fourWeAreChecking = line.substring(i,i+4);
+            char[] chars = fourWeAreChecking.toCharArray();
+            for(int p = 0; p <chars.length; p++){
+                for(int q = p+1; q < chars.length; q++){
+                    if(chars[p] == chars[q]){
+                        continue outerLoop;
+                    }
+                }
+            }
+            System.out.println(i+4);
+            break;
+         }
+         
+            
+        }
 
-    }
+    
 
     public static void main(String...args) {
         try {
